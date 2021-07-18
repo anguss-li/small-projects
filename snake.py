99da24c826c783c6f7b1aecded44db3ed897b03b
Angus Li <angusr.li02@gmail.com>
2021-07-18 23:07:04 +0800

    Fixed Snake indexing bug and documentation

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 from random import randint
 from time import sleep
+from tkinter.constants import N, NO
 from turtle import Turtle, TurtleScreen, _Screen
 
 
@@ -8,6 +9,9 @@ class GamePart(Turtle):
         '''
         Used for interactive objects in game. Inherited by classes SnakePart, 
         ScoreCounter and Food.
+
+        shape: valid polygon shape in turtle.
+        color: valid color name in turtle. 
         '''
         super().__init__()
         self.speed(0)
@@ -26,11 +30,9 @@ class Food(GamePart):
         self.shapesize(0.50, 0.50)
         self.goto(0, 0)
 
-    def teleport(self, x_limit, y_limit):
+    def teleport(self, x_limit, y_limit) -> None:
         '''
-        x_limit, y_limit: integers, maximum distance food can travel in x and y 
-        axes respectively.
-        Returns: None, randomly moving Food object to location on game board.
+        Randomly moves self to location on game board.
         '''
         x_pos = randint(-(x_limit), x_limit)
         y_pos = randint(-(y_limit), y_limit)
@@ -40,10 +42,9 @@ class Food(GamePart):
 class ScoreCounter(GamePart):
     def __init__(self, font, x_pos, y_pos):
         '''
-        Dummy object used by Board. Used to write Board.score and 
-        Board.high_score on screen.
+        Dummy object used to write Board.score and Board.high_score on screen.
 
-        font: string, valid font in turtle.
+        font: valid font in turtle.
         '''
         super().__init__(shape="square", color="black")
         self.font = font
@@ -51,37 +52,38 @@ class ScoreCounter(GamePart):
         self.goto(x_pos, y_pos)
         self.score = 0
 
-    def get_high_score(self):
+    def get_high_score(self) -> int:
         '''
-        Returns: integer, value of high_score according to 'high_score.txt'.
+        Returns value of high_score according to 'high_score.txt'.
         '''
         high_score_record = open('high_score.txt', 'r')
-        return int(float(high_score_record.read()))
+        return int(high_score_record.read())
 
-    def set_high_score(self, high_score):
+    def set_high_score(self, high_score) -> None:
         '''
-        high_score: integer, new value of self.high_score
-        Returns: None, overwriting self.high_score with high_score.
+        Sets self.high_score to input.
         '''
         high_score_record = open('high_score.txt', 'w')
         high_score_record.write(str(high_score))
 
-    def change_score(self, score_change):
+    def change_score(self, score_change) -> int:
         '''
-        score_change: integer, number by which self.score will be modified
-        Returns: new value of score, as well as a new high score if new score is 
-        higher than the previous high score
+        Returns new value of score, as well as a new high score if new score is 
+        higher than the previous high score.
+
+        score_change: number by which self.score will be modified
         '''
         high_score = self.get_high_score()
         self.score += score_change
         if self.score > high_score:
             self.set_high_score(self.score)
 
-    def write_score(self):
+    def write_score(self) -> None:
         '''
-        score: integer, score of current game
+        Writes in text on board values of score and high_score.
+        
+        score: score of current game
         high_score: highest value of score over all previous games
-        Returns: None, writing in text on board values of score and high_score
         '''
         score, high_score = self.score, self.get_high_score()
         self.clear()
@@ -90,24 +92,24 @@ class ScoreCounter(GamePart):
 
 
 class Board(_Screen):
-    def __init__(self, title, color, width, height, font):
+    def __init__(self, title, color, length, font):
         '''
         Implements game window as well as elements not controlled by the user.
 
-        title: string, text to be displayed as window title
-        color: string, valid turtle color to be used as background
-        width, height: integers, width and height in pixels respectively
-        font: string, valid font in turtle to be used for writing
+        title: text to be displayed as window title
+        color: valid turtle color to be used as background
+        length: length of board in pixels
+        font: valid font in turtle to be used for writing
         '''
         super().__init__()
         TurtleScreen.__init__(self, Board._canvas)
         Turtle._screen = self
         self.title(title)
         self.bgcolor(color)
-        self.setup(width=width, height=height)
+        self.setup(width=length, height=length)
         self.tracer(0)
         self.food = Food()
-        self.score_counter = ScoreCounter(font, 0, (height / 2) - 40)
+        self.score_counter = ScoreCounter(font, 0, (length / 2) - 40)
 
 
 class SnakePart(GamePart):
@@ -115,7 +117,7 @@ class SnakePart(GamePart):
         '''
         Dummy object used by snake.
 
-        x_pos, y_pos: integers, x and y coordinates respectively.
+        x_pos, y_pos: x and y coordinates respectively.
         '''
         super().__init__(shape="square", color="black")
         self.goto(x_pos, y_pos)
@@ -129,41 +131,40 @@ class Snake(list):
         interactive element of the game.
         '''
         super().__init__()
-        self.insert(0, SnakePart(0, 100))
+        self.append(SnakePart(0, 100))
         self.direction = "stop"
 
-    def add_part(self, x_pos, y_pos):
+    def add_part(self, x_pos, y_pos) -> None:
         '''
+        Appends a new SnakePart at x_pos and y_pos.
+
         x_pos, y_pos: x and y coordinates of the Food object eaten by snake
-        Returns: None, appending to self a new SnakePart at x_pos and y_pos
         '''
         self.insert(0, SnakePart(x_pos, y_pos))
 
-    def delete_part(self, index):
+    def delete_part(self, part: SnakePart) -> None:
         '''
-        index: integer, index of part in snake to be deleted
-        Returns: None, hiding self[index] before deleting it from structure
-        '''
-        self[index].hideturtle()
-        self.pop(index)
+        Hides part before deleting it from structure.
 
-    def reset(self):
+        part: part in snake to be deleted
         '''
-        Returns: None, deleting every part of the snake besides self[0], 
-        resetting position and setting direction to "stop".
+        part.hideturtle()
+        self.remove(part)
+
+    def reset(self) -> None:
         '''
-        for part in range(1, len(self)):
-            try:
-                self.delete_part(part)
-            except IndexError:
-                pass
+        Return self to default configuration.
+        '''
+        snake_ref = self.copy()
+        for part in snake_ref:
+            if snake_ref.index(part) != 0:
+                self.delete_part(part)  
         self[0].goto(0, 100)
         self.direction = "stop"
 
-    def move(self):
+    def move(self) -> None:
         '''
-        Returns: None, setting movement direction of the snake according to
-        self.direction
+        Sets movement direction of self according to self.direction.
         '''
         x_pos = self[0].xcor()  # x coordinate of the turtle
         y_pos = self[0].ycor()  # y coordinate of the turtle
@@ -176,68 +177,68 @@ class Snake(list):
         elif self.direction == "left":
             x_pos -= 20
         self.add_part(x_pos, y_pos)
-        self.delete_part(-1)
+        self.delete_part(self[-1])
 
-    def go_up(self):
+    def go_up(self) -> None:
         '''
-        Returns: None, setting direction of the head to "up" if not currently 
-        "down"
+        Sets direction of the head to "up" if not currently "down".
         '''
         if self.direction != "down":
             self.direction = "up"
 
-    def go_down(self):
+    def go_down(self) -> None:
         '''
-        Returns: None, setting direction of the head to "down" if not currently 
-        "up"
+        Sets direction of the head to "down" if not currently "up".
         '''
         if self.direction != "up":
             self.direction = "down"
 
-    def go_right(self):
+    def go_right(self) -> None:
         '''
-        Returns: None, setting direction of the head to "right" if not currently 
-        "left"
+        Sets direction of the head to "right" if not currently "left".
         '''
         if self.direction != "left":
             self.direction = "right"
 
-    def go_left(self):
+    def go_left(self) -> None:
         '''
-        Returns: None, setting direction of the head to "left" if not currently 
-        "right"
+        Sets direction of the head to "left" if not currently "right".
         '''
         if self.direction != "right":
             self.direction = "left"
 
 
 class Game(object):
-    def __init__(self, title, color, width, height, font):
+    def __init__(self, title: str, color: str, length: int, font: str, delay: float):
         '''
         Used to implement main game functions, such as main loop and game over
         conditions.
 
-        title: string, text to be displayed as window title
-        color: string, valid turtle color to be used as background
-        width, height: integers, width and height in pixels respectively
-        font: string, valid font in turtle to be used for writing
+        title: text to be displayed as window title
+        color: valid turtle color to be used as background
+        length: how long the board should be in pixels
+        font: valid font in turtle to be used for writing
+        delay: time step in between actions in game
         '''
-        self.board = Board(title, color, width, height, font)
+        self.board = Board(title, color, length, font)
         self.snake = Snake()
+        self.delay = delay
         self.score = 0
 
-    def game_over(self):
+    def game_over(self) -> None:
         '''
-        Returns: None, resetting snake, score and food.
+        Resets snake, score and food.
         '''
         sleep(1)
         self.snake.reset()
-        self.board.score_counter.change_score(-(self.board.score))
-        self.board.food.goto(0, 0)
+        score_counter = self.board.score_counter
+        food = self.board.food
+        score_counter.change_score(-(score_counter.score))
+        food.goto(0, 0)
 
-    def run(self, delay):
+    def run(self) -> None:
         '''
-        Returns: None, setting up main game loop.
+        Sets up main game loop.
         '''
         board = self.board
         snake = self.snake
@@ -254,7 +255,8 @@ class Game(object):
             board.update()
             score_counter.write_score()
             snake.move()
-            sleep(delay)
+            # print(snake)
+            sleep(self.delay)
             head = snake[0]
             if head.distance(food) < 15:
                 # Increase score, check for high score increase
@@ -276,7 +278,6 @@ class Game(object):
             if has_snake_collided:
                 self.game_over()
 
-
-game = Game("Angus' Snake Game", "MintCream", 800, 800, "Courier")
-
-game.run(0.125)
+if __name__ == '__main__':
+    game = Game("Angus' Snake Game", "MintCream", 800, "Courier", 0.125)
+    game.run()
