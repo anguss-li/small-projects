8b46cf17fc3adcc6d14ff8d8dc3253e41c87fb93
Angus Li <angusr.li02@gmail.com>
2021-08-25 01:26:59 +0800

    Finalized widescreen support; cleaned classes

--- snake.py
+++ snake.py
@@ -40,7 +40,7 @@ class Food(GamePart):
 
 
 class Board(_Screen):
-    def __init__(self, title: str, color: str, length: int, font: str):
+    def __init__(self, title: str, color: str, width: int, height: int):
         '''
         Implement game window as well as elements not controlled by the user.
 
@@ -54,7 +54,7 @@ class Board(_Screen):
         Turtle._screen = self
         self.title(title)
         self.bgcolor(color)
-        self.setup(width=length, height=length)
+        self.setup(width=width, height=height)
         self.tracer(0)
 
 
@@ -78,6 +78,11 @@ class Snake(list):
         super().__init__()
         self.head = SnakePart(0, 100)
         self._direction = 'stop'
+        self.is_direction_legal = {'up': self.direction != 'down',
+                                   'down': self.direction != 'up',
+                                   'left': self.direction != 'right',
+                                   'right': self.direction != 'left',
+                                   'stop': True}
 
     @property
     def direction(self) -> str:
@@ -85,12 +90,7 @@ class Snake(list):
 
     @direction.setter
     def direction(self, direction: str) -> None:
-        is_legal = {'up': self.direction != 'down',
-                    'down': self.direction != 'up',
-                    'left': self.direction != 'right',
-                    'right': self.direction != 'left',
-                    'stop': True}
-        if is_legal[direction]:
+        if self.is_direction_legal[direction]:
             self._direction = direction
 
     def move(self, step: int) -> None:
@@ -199,15 +199,15 @@ class SnakeGame(object):
         delay: time step in between actions in game
         '''
         self.width = width
-        self.length = height
+        self.height = height
         self.delay = delay
         self.score = 0
-        self.board = Board(title, color, height, font)
+        self.board = Board(title, color, self.width, self.height)
         self.snake = Snake()
         self.food = Food()
-        self.score_counter = ScoreCounter(font, 0, (height/2)-40)
-        self.step = self.length // 40
-        self.limits = (self.width // 2, self.length // 2)
+        self.score_counter = ScoreCounter(font, 0, (self.height/2)-40)
+        self.step = self.height // 40
+        self.limits = (self.width // 2, self.height // 2)
 
     def initialize(self) -> None:
         '''Create main game components'''
@@ -230,21 +230,21 @@ class SnakeGame(object):
         def win_points() -> None:
             '''Account for food being eaten.'''
             self.score_counter.change_scores(10)
-            [temp_x, temp_y] = list(map(lambda x: x + self.step*2, self.limits))
+            [temp_x, temp_y] = [pos + self.step*2 for pos in self.limits]
             self.snake.append(SnakePart(temp_x, temp_y))
             self.food.teleport(self.limits)
 
         def is_pos_legal() -> None:
             '''Check that snake is in bounds and is not eating itself'''
-            snake, head, step = self.snake, self.snake.head, self.step
-            is_eaten = any(part.distance(head) < step for part in snake)
-            is_bounded = all(-l < p < l for p,
-                             l in zip(head.pos(), self.limits))
+            is_eaten = any(part.distance(self.snake.head) < self.step
+                           for part in self.snake)
+            is_bounded = all(-l < p < l
+                             for p, l in zip(self.snake.head.pos(), self.limits))
             return not is_eaten and is_bounded
 
         def game_over() -> None:
             '''Reset snake, score and food.'''
-            sleep(1)
+            sleep(self.delay * 8)
             self.snake.reset()
             self.score_counter.change_scores(-self.score_counter.score)
             self.food.goto(0, 0)
@@ -263,7 +263,7 @@ if __name__ == '__main__':
     game = SnakeGame("Angus' Snake Game",
                      'MintCream',
                      'Courier',
-                     800,
+                     1200,
                      800,
                      0.125)
     game.initialize()
